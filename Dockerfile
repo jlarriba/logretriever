19f814db312d28a8e7e95f2e90b8a025bb7d8f3d
Juan Larriba <juan@jlarriba.es>
2019-08-02 16:00:48 +0200

    First push

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+#docker build -t jlarriba/logretriever:0.2 .
+FROM golang:1.7
+RUN mkdir -p /main
+WORKDIR /main
+ADD . /main
+RUN go build ./main.go
+CMD ["./main"]
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	exportFolder string
+	logsFolder   string
+)
+
+func main() {
+fmt.Printf("Logging Main process \n")
+	//logsFolder = os.Getenv("LOGS_FOLDER")
+	//exportFolder = os.Getenv("EXPORT_FOLDER")
+	interval, err := strconv.Atoi(os.Getenv("EXEC_INTERVAL"))
+	if err != nil {
+		interval = 7
+	}
+	if logsFolder == "" {
+		logsFolder = "/var/log/containers"
+	}
+	if exportFolder == "" {
+		exportFolder = "/opt/logs"
+	}
+	for {
+		fmt.Println("Gathering logs...")
+		err2 := filepath.Walk(logsFolder, visit)
+		if err2 != nil {
+			fmt.Println("Error visiting file", err2)
+		}
+		fmt.Printf("Pausing the process during %d minutes \n", interval)
+		time.Sleep(time.Minute * time.Duration(interval))
+	}
+}
+
+func visit(path string, f os.FileInfo, err error) error {
+	if f != nil && !strings.Contains(f.Name(), "POD") {
+		if f.IsDir() {
+			fmt.Println(path, "is a directory, ignore")
+			return nil
+		}
+		realPath, errSymLink := filepath.EvalSymlinks(path)
+		if errSymLink != nil {
+			fmt.Println(path, " is not a SymLink")
+			realPath = path
+		}
+
+		sName := strings.Split(f.Name(), "_")
+		if (sName[1] != "kube-system") {
+			sNamespace := strings.Split(sName[1], "-")
+			if len(sNamespace) != 2 {
+				err = copy(realPath, exportFolder+"/"+sNamespace[0]+"/pro/"+sName[2])
+			} else {
+				err = copy(realPath, exportFolder+"/"+sNamespace[0]+"/"+sNamespace[1]+"/"+sName[2])
+			}
+	
+			if err != nil {
+				fmt.Println("Error copying file", realPath, err)
+			}
+		} else {
+			fmt.Println(sName[1], "Ignoring namespace")
+		}
+	}
+	return err
+}
+
+func copy(src, dst string) error {
+	fmt.Printf("Opening file to be copied %s \n", src)
+	in, err := os.Open(src)
+	if err != nil {
+		in.Close()
+		return err
+	}
+	//defer in.Close() ==>Avoiding to use defer in order to close files
+	fmt.Printf("Creating destination file %s \n", dst)
+	os.MkdirAll(filepath.Dir(dst), os.ModePerm)
+	out, err := os.Create(dst)
+	if err != nil {
+		in.Close()
+		out.Close()
+		return err
+	}
+	//defer out.Close() ==>Avoiding to use defer in order to close files
+	fmt.Printf("Copying from %s to %s\n", src, dst)
+	bytesWritten, err := io.Copy(out, in)
+	if err != nil {
+		in.Close()
+		out.Close()
+		return err
+	}
+	fmt.Printf("Copied %d bytes. \n", bytesWritten)
+	fmt.Printf("Copied from %s to %s\n", src, dst)
+	//cerr := out.Close()
+	//return cerr
+	in.Close()
+	out.Close()
+	return err
+}
